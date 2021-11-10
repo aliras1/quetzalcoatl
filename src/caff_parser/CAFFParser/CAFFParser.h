34dc46299abfebed6c8164998db9162b02849420
Kálmán Mozsolics <mazsolakalman@gmail.com>
2021-11-10 15:30:40 +0100

    Add caption and tags parsing ,add CAFF and CIFF structs

--- src/caff_parser/CAFFParser/CAFFParser.h
+++ src/caff_parser/CAFFParser/CAFFParser.h
@@ -1,7 +1,23 @@
 #include "ByteReader.h"
+#include <vector>
 using namespace std;
 using namespace Orionark::Utility;
 
+struct CAFF
+{
+	//meg ideel lehetne menteni dolgokat
+	vector<CIFF> ciffs;
+};
+
+struct CIFF {
+	int id;
+	long long int contentSize;
+	long long int width;
+	long long int height;
+	string caption;
+	vector<string> tags;
+	//meg ide amibe a kepet magat taroljuk 
+};
 
 class CAFFParser
 {
@@ -9,14 +25,14 @@ class CAFFParser
 public:
 
 
-	bool parser(char* data, size_t size) {
+	CAFF parser(char* data, size_t size) {
 		ByteReader bytereader(data, size);
 		bytereader.SetEndian(LITTLE_ENDIAN);
 
 		//Caff header beolvasása
 		char CAFFheaderId = bytereader.ReadByte();
 		if (CAFFheaderId != 1) {
-			return false;
+			//return false;
 		}
 		long long int CAFFheaderLength = bytereader.ReadInt64();
 		char c;
@@ -40,17 +56,51 @@ public:
 		long long int CAFFAnimationHeaderLength = bytereader.ReadInt64();
 		long long int duration = bytereader.ReadInt64();
 		//Ciffek beolvasasa
+		vector<CIFF> ciffs;
 		for (int i = 0; i < numberOfCIFF; i++) {
+			CIFF ciff;
+			ciff.id = i;
 			string ciff = bytereader.ReadString(4);
 			long long int CiffHeaderSize = bytereader.ReadInt64();
-			long long int contentSize = bytereader.ReadInt64();
-			long long int width = bytereader.ReadInt64();
-			long long int height = bytereader.ReadInt64();
-			if (contentSize != width * height * 3)
-				return false;
-			//itt meg kell csinalni meg a captiont 
-			//meg kell csinalni meg a tagseket 
+			ciff.contentSize = bytereader.ReadInt64();
+			ciff.width = bytereader.ReadInt64();
+			ciff.height = bytereader.ReadInt64();
+			if (ciff.contentSize != ciff.width * ciff.height * 3) {
+				//	return false;
+			}
+			//caption
+			char ch;
+			string caption;
+			int n = 0;
+			while (ch != '\n') {
+				ch = NULL;//ez nem biztos hogy kell
+				ch = bytereader.ReadByte();
+				n++;
+				caption.push_back(ch);
+			}
+			ciff.caption = caption;
+			//tags
+			long long int tagsLength = CiffHeaderSize - 4 - 8 - 8 - 8 - 8 - n;
+			int k = 0;
+			vector<string> tags;
+			string tag;
+			for (int i = 0; i < tagsLength; i++) {
+				tag.clear();
+				while (ch != '\0') {
+					ch = NULL;
+					k++;
+					ch= bytereader.ReadByte();
+					tag.push_back(ch);
+				}
+				tags.push_back(tag);
+				tagsLength -= k;
+			}
+			ciff.tags = tags;
+
 			// at kell konvertalni valahogy a bytokat keppe
+
+
+			ciffs.push_back(ciff);
 		}
 	}
 };
