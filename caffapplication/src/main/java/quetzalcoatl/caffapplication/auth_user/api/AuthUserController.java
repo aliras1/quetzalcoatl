afcb80eb65c088ad3f48e8750a0f4990ae265c8c
Kovács Bence <kovacs.bence@effectivegroup.hu>
2021-11-25 19:55:12 +0100

    #create admin

--- caffapplication/src/main/java/quetzalcoatl/caffapplication/auth_user/api/AuthUserController.java
+++ caffapplication/src/main/java/quetzalcoatl/caffapplication/auth_user/api/AuthUserController.java
@@ -1,13 +1,17 @@
 package quetzalcoatl.caffapplication.auth_user.api;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import quetzalcoatl.caffapplication.auth_user.AuthUser;
 import quetzalcoatl.caffapplication.auth_user.AuthUserRepository;
+import quetzalcoatl.caffapplication.auth_user.AuthUserService;
 import quetzalcoatl.caffapplication.base.auth.Role;
 import quetzalcoatl.caffapplication.base.auth.RoleSecured;
 
@@ -19,9 +23,11 @@ import java.util.stream.Collectors;
 public class AuthUserController {
 
     private final AuthUserRepository authUserRepository;
+    private final AuthUserService authUserService;
 
-    public AuthUserController(AuthUserRepository authUserRepository) {
+    public AuthUserController(AuthUserRepository authUserRepository, AuthUserService authUserService) {
         this.authUserRepository = authUserRepository;
+        this.authUserService = authUserService;
     }
 
     @GetMapping("/getAuthenticatedUser")
@@ -33,4 +39,11 @@ public class AuthUserController {
         Set<Role> roles = authentication.getAuthorities().stream().map(ga -> Role.valueOf(ga.getAuthority())).collect(Collectors.toSet());
         return new AuthenticatedUserResponseDTO(loggedInUser, roles);
     }
+
+    @PostMapping
+    public ResponseEntity<Object> post(@RequestBody CreateUserRequestDTO requestDTO) {
+        authUserService.create(requestDTO);
+        return ResponseEntity.ok().build();
+    }
+
 }
---
+++ caffapplication/src/main/resources/database/03_create_admin_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO auth_user(id, username, password_hash)
+VALUES (1, 'admin', '{bcrypt}$2a$10$vVQgOzqgfBgEGNiZ14Wss.zGhRJGmoB4SMnNzkXr7IYsAkaNGYOa2');
