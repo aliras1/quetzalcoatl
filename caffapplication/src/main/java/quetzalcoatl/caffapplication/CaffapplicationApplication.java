e9650c7b47a1a50efa57382825728a2b0917b7c0
Kálmán Mozsolics <mazsolakalman@gmail.com>
2021-11-25 14:18:32 +0100

    Init

---
+++ caffapplication/src/main/java/quetzalcoatl/caffapplication/CaffapplicationApplication.java
@@ -0,0 +1,13 @@
+package quetzalcoatl.caffapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffapplicationApplication.class, args);
+	}
+
+}
