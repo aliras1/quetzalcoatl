e6cfc7a7077f6a51f115e762e3e99bc1f2a1cc76
Kovács Bence <kovacs.bence@effectivegroup.hu>
2021-11-25 22:20:44 +0100

    #sql bug fix

--- caffapplication/src/main/resources/database/02_create_auth_user_roles.sql
+++ caffapplication/src/main/resources/database/02_create_auth_user_roles.sql
@@ -1,7 +1,7 @@
-CREATE TABLE user_roles
+CREATE TABLE auth_user_roles
 (
-    user_id BIGINT       NOT NULL,
+    auth_user_id BIGINT       NOT NULL,
     roles        VARCHAR(256) NOT NULL,
-    PRIMARY KEY (user_id, roles),
-    FOREIGN KEY (user_id) REFERENCES auth_user(id)
+    PRIMARY KEY (auth_user_id, roles),
+    FOREIGN KEY (auth_user_id) REFERENCES auth_user(id)
 );
